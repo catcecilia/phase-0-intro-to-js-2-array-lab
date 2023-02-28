fc5967c4d0367725baf81900b3ae7e9da33f7f37
Phuong-Cat Ngo <phuongcatngo@gmail.com>
2023-02-28 11:44:21 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(cat){
+    cats.push(cat);
+}
+
+function destructivelyPrependCat(cat){
+    cats.unshift(cat);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(cat){
+    return [...cats, cat];
+}
+
+function prependCat(cat){
+    return [cat, ...cats];
+}
+
+function removeLastCat(){
+    return cats.slice(0,cats.length-1);
+}
+function removeFirstCat(){
+    return cats.slice(1);
+}
